848e47c3ce796ace331290d8f777b3ca5e984620
queragion2726 <jms100300@naver.com>
2018-05-22 23:57:14 +0900

    Update example.cpp

--- src/example.cpp
+++ src/example.cpp
@@ -10,7 +10,7 @@ int main(void) {
         array[i][i][i] = 1;
     }
 
-    // Print Qube!!
+    // Print Cube!!
     for (int i = 0; i < 3; i ++) {
         for (int j = 0; j < 3; j ++) {
             for (int k = 0; k < 3; k ++)
